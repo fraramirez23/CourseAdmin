a3db9062571bdbe304526ccad60f200132d67936
Francis Ricardo Ramirez <francisricardor@gmail.com>
2021-02-01 19:47:17 -0400

    PCM-2021 #Comment Se crearon las pantallas de creacion de cursos y listado.

---
+++ CourseAdm/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CourseAdm.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Descripction { get; set; }
+        public string  Area { get; set; }
+    }
+}
